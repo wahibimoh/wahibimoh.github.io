067a2b3b2eb05c3c48229a1977b4c8410caa5f1a
Alwahibi <m3dteam@gmail.com>
2023-06-22 15:11:33 +0300

    Update test.js

--- test.js
+++ test.js
@@ -6,19 +6,19 @@ var selectedElement = null;
 window.res = document.querySelectorAll('div'); 
 
 for(div of window.res) 
-	if(div.innerHTML == 'test_123') 
+	if(div.innerHTML == 'Approval Counter: Click Me') 
 		selectedElement = div
 	
 console.log("selectedElement",selectedElement)
-	
+var counter = 0;	
 function time() {
   var d = new Date();
   var s = d.getSeconds();
   var m = d.getMinutes();
   var h = d.getHours();
-  selectedElement.innerHTML = 
-    ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+  //selectedElement.innerHTML = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+	selectedElement.innerHTML = "Approval Counter:"+ counter++;
 }
 
-setInterval(time, 1000);
+setInterval(time, 5000);
 })();
